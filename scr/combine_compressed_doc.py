69b6c1bdb16bbe5766ba60172fb0e123d3c97548
Jasmine Jin <jinxiting2002@gmail.com>
2023-08-09 14:25:15 -0400

    modified the file so the index also merges

--- scr/combine_compressed_doc.py
+++ scr/combine_compressed_doc.py
@@ -3,6 +3,10 @@ import pandas as pd
 df1 = pd.read_csv('../alchemy_texts/texts_aka_compressed_documents.csv')
 df2 = pd.read_csv('../alchemy_texts/texts_aka_compressed_documents2.csv')
 
+starting_index = df1['Unnamed: 0'].iloc[-1] + 1
+
+df2["Unnamed: 0"] = df2.apply(lambda row: int(row["Unnamed: 0"]) + starting_index, axis=1)
+
 combined_df = pd.concat([df1, df2], ignore_index=True)
 
 combined_df.to_csv("combined_compressed_doc.csv", index=False)
